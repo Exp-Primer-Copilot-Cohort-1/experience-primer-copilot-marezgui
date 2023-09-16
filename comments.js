fedb89aa5f759a9ea928a4c70096dc813ca6ed45
Malek Rezgui <m.rezgui@outlook.fr>
2023-09-16 22:39:48 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,35 @@
+// Create web server
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// Create app
+const app = express();
+// Middleware
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Create route
+app.get('/posts/:id/comments', (req, res) => {
+  res.send([
+    {
+      id: 1,
+      content: 'Comment 1',
+    },
+    {
+      id: 2,
+      content: 'Comment 2',
+    },
+    {
+      id: 3,
+      content: 'Comment 3',
+    },
+  ]);
+});
+
+// Listen to port
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+});
